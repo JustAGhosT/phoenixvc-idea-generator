08114f49ff7c838d51db675b032a7dd54e0ebcc3
JustAGhosT <smit.jurie@gmail.com>
2025-04-30 19:41:50 +0200

    refactor(storybook): remove unused docs configuration and update path aliases

--- .storybook/main.js
+++ .storybook/main.js
@@ -16,9 +16,6 @@ module.exports = {
     name: '@storybook/react-webpack5',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
   webpackFinal: async (config) => {
     // Add TypeScript support
     config.resolve.extensions.push('.ts', '.tsx');
